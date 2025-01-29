51557849632b48b793c3d0b7e34b5504a3ac5359
MIHIR PATADIYA <mihir.patadiya@icloud.com>
2025-01-29 20:22:55 +0530

    Delete ADVANCE PHP CONCEPTS directory

--- ADVANCE PHP CONCEPTS/IsPalindrome.php
+++
@@ -1,9 +0,0 @@
-<?php
-function isPalindrome($str) {
-    $str = strtolower(preg_replace("/[^a-zA-Z0-9]/", "", $str));
-    return $str == strrev($str);
-}
-
-// Example usage
-echo isPalindrome("madam") ? "Palindrome" : "Not Palindrome";
-?>
